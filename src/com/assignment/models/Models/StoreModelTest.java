ea20abaccf8da13668ca04a11faf9c96a15706a3
Patrik Varga <patrikadamvarga@gmail.com>
2019-05-19 23:13:04 +0200

    added dependencies to repo

---
+++ src/com/assignment/models/Models/StoreModelTest.java
@@ -0,0 +1,22 @@
+package com.assignment.models.Models;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StoreModelTest {
+
+    @org.junit.jupiter.api.Test
+    void getTotalPrice() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addToShoppingListById() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeFromShoppingListById() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllWares() {
+    }
+}
