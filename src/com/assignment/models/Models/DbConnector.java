1b720c7b04405ad926a6a762f56aa7998b40b094
Patrik Varga <patrikadamvarga@gmail.com>
2019-05-21 11:13:16 +0200

    db moved to online host

--- src/com/assignment/models/Models/DbConnector.java
+++ src/com/assignment/models/Models/DbConnector.java
@@ -8,9 +8,9 @@ public class DbConnector {
     }
     private static Connection connect;
 
-    private static final String URL = "jdbc:mysql://localhost/storeappdb";
-    private static final String USER = "storeappdb";
-    private static final String PASS = "storeappdb";
+    private static final String URL = "jdbc:mysql://nr84dudlpkazpylz.chr7pe7iynqr.eu-west-1.rds.amazonaws.com/rxz66kautu2nsm7f";
+    private static final String USER = "pbsv70h8vfu5f9hq";
+    private static final String PASS = "hbhv0lsicraakzpk";
 
     /**
      * Creates connection to the database
@@ -24,6 +24,7 @@ public class DbConnector {
                 Class.forName("com.mysql.jdbc.Driver");
                 connect = DriverManager.getConnection(URL, USER, PASS);
             } catch (SQLException e) {
+                e.printStackTrace();
                 throw e;
             } catch (ClassNotFoundException e) {
                 throw e;
